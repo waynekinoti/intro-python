08fdde2fb9165430ede3efbc553cc25b039703de
Wayne <waynemurimi01@gmail.com>
2025-03-20 13:29:52 +0300

    learned dictionary

--- loops3.py
+++ loops3.py
@@ -24,3 +24,6 @@ for num in range(lower, upper + 1):
            # Iterate 10 times from i = 1 to 10
            for i in range(1, 11):
                print(num, 'x', i, '=', num * i)
+
+for number in range(100,0,-1):
+    print(number)
