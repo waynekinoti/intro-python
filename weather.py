0e9f09a9a28586d309a2c3e6fe86817eba2d26d6
Wayne <waynemurimi01@gmail.com>
2025-04-02 12:47:14 +0300

    learned json weather

--- weather.py
+++ weather.py
@@ -1,20 +1,24 @@
 import json
+from datetime import datetime
 
-file = open("weather.json", "r")
+file = open('weather.json', "r")
 
 data = file.read()
 # print(type(data))
 json_data = json.loads(data)
 # print(type(json_data))
 
-print(json_data['main']['temp'])
+# print(json_data['main']['temp'])
 
 weather = json_data['weather'][0]['main']
-print(weather)
+# print(weather)
 
-print(json_data['wind']['speed'])
+# print(json_data['wind']['speed'])
 
 # sunrise
 # fomart and print 10:30PM
 # epoch
 
+sunrise_time = json_data['sys']['sunrise']
+value = datetime.fromtimestamp(sunrise_time).strftime('%H:%M')
+print(value)
